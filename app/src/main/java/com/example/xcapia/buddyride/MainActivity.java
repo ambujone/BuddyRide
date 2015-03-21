be01c9b65ab6a3b7f2b43e398783e0069600ba1c
Ambuj Agrawal <ambujone@gmail.com>
2015-03-21 18:30:49 +0000

    Added driver button testing

--- app/src/main/java/com/example/xcapia/buddyride/MainActivity.java
+++ app/src/main/java/com/example/xcapia/buddyride/MainActivity.java
@@ -81,9 +81,9 @@ public class MainActivity extends FragmentActivity implements
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
     }
